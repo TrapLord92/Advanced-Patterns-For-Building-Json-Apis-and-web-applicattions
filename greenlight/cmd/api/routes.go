8b9283a28a59cecaaa1df9ce18fa6f9d7357655f
TrapLord92 <traplord345@gmail.com>
2024-10-05 02:42:03 +0500

    Parsing Query String Parameters

--- greenlight/cmd/api/routes.go
+++ greenlight/cmd/api/routes.go
@@ -16,6 +16,8 @@ func (app *application) routes() *httprouter.Router {
 	// it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	// Add the route for the GET /v1/movies endpoint.
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.
 		showMovieHandler)
