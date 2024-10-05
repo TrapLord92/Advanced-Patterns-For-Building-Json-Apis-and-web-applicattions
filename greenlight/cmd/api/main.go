385e05ff2ae8925fbf5a1e2365de04d96a6eedba
TrapLord92 <traplord345@gmail.com>
2024-10-06 04:09:02 +0500

    Graceful Shutdown of Background

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	// New import
@@ -46,6 +47,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
