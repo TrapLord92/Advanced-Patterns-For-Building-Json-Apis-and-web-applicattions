062d841f4e1fb89745344b68228f65396db49c64
TrapLord92 <traplord345@gmail.com>
2024-10-07 00:10:34 +0500

    Requiring User Activation

--- greenlight/cmd/api/routes.go
+++ greenlight/cmd/api/routes.go
@@ -6,24 +6,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// Update the routes() method to return a http.Handler instead of a *httprouter.Router.
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
-	// Add the route for the POST /v1/users endpoint.
+	// Use the requireActivatedUser() middleware on our five /v1/movies** endpoints.
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requireActivatedUser(app.listMoviesHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requireActivatedUser(app.createMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requireActivatedUser(app.showMovieHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requireActivatedUser(app.updateMovieHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requireActivatedUser(app.deleteMovieHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
-	// Add the route for the PUT /v1/users/activated endpoint.
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
-	// Add the route for the POST /v1/tokens/authentication endpoint.
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	// Wrap the router with the panic recovery middleware.
-	// Use the authenticate() middleware on all requests.
 	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
