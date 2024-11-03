8b54c76c9a83f3956f46eb426bb3c407919cee4a
TrapLord92 <traplord345@gmail.com>
2024-11-04 03:31:29 +0500

    pipeline with groovy

--- greenlight/buildDockerImage.groovy
+++ greenlight/buildDockerImage.groovy
@@ -1,9 +1,14 @@
+// buildDockerImage.groovy
+
 def call(String credentialsId, String dockerFile, String imageName) {
     echo "Building the Docker image in directory 'greenlight'"
     
     dir('greenlight') {
         withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            // Build Docker image
             sh "docker build -f ${dockerFile} -t ${imageName} ."
+
+            // Login and push the Docker image
             sh "echo \$PASS | docker login -u \$USER --password-stdin"
             sh "docker push ${imageName}"
         }
